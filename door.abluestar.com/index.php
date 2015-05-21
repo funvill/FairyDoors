d5002854ce9123356bfe0822660d9c6cf8d222a4
Steven Smethurst <steven@abluestar.com>
2015-05-20 19:22:50 -0700

    Splitting up the comment system

--- door.abluestar.com/index.php
+++ door.abluestar.com/index.php
@@ -1,2 +1,31 @@
-This is from github. 
-This is an automatic update from the web server. 
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Magic doors</title>
+    <meta name="description" content="Magic doors project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+  <body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <!-- Top nav -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <h1>Magic Doors</h1>
+        </div>
+      </div>
+    </nav>
+
+	<div class="container">
+		<p>this is some information</p>
+	</div>
+
+	<footer>
+		<a href='#'>abluestar.com</a>
+	</footer>	
+</body>
+</html>
