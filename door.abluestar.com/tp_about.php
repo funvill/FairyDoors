0b243d47ae231ead50b7a3a6a2156956b235c1ba
Steven Smethurst <steven@abluestar.com>
2015-06-30 00:40:23 -0700

    Adding content to the about page.

--- door.abluestar.com/tp_about.php
+++ door.abluestar.com/tp_about.php
@@ -1,21 +1,37 @@
-
-  <div class="jumbotron">
-    <h1>Magic doors</h1>
-    <p>As we all know, little doors are portals to magical places. They can lean one to gnomes, fairies, and many other types of magical creatures!</p>
+<div class='page'>
+  <div class="container">
+    <div class='col-md-12'>
+      <h1>Magic doors</h1>
+      <p>As we all know, little doors are portals to magical places. They can lean one to gnomes, fairies, and many other types of magical creatures!</p>
+    </div>
   </div>
 
   <div class="container">
-    <div class="row">
+    <div class='col-md-12'>
       ToDo: More info and example doors.
-      ToDo: List sponsors VHS, Vancouver City, Mount plesnese, etc...
     </div>
   </div>
 
 
+  <div class="container">
+    <div class='col-md-12' >
+      <h3>Sponsors</h3>
+      <p>This project was sponsored by the following organizations<p>
+      <a href='https://vancouver.hackspace.ca/wp/'><img src='img/vhslogo.png' alt='The Vancouver Hackspace' /></a>
+      <a href='http://www.mpnh.org/'><img src='img/mpnh.png' alt='Mount Pleasant Neighbourhood House' /></a>
+      <a href='https://www.vancouverfoundation.ca/'><img src='img/vancouverFoundation.png' alt='The Vancouver Foundation' /></a>
+    </div>
+  </div>
 
   <div class="container">
-    <div class="row">
-      <h3>Contact me</h3>
+    <div class='col-md-6' >
+      <h3>About Steven Smethurst</h3>
+      <p><img src='#' width='200px' height='200px' align='left' style='margin: 0 20px 20px 0' /> Steven Smethurst is a Vancouver based Maker, Hacker, Coder, and all around great guy! <br />He created this project to bring a little more magic and happiness into this world. <br />You can find more of his projects at his blog <a href='http://blog.abluestar.com/'>abluestar.com</a></p>
+    </div>
+
+    <div class='col-md-6' >
+      <h3>Contact</h3>
+      <p>You can add or contact Steven Smethurst via the following social media sites.</p>
       <ul class="social">
         <li><a href="https://twitter.com/funvill"><i class="fa fa-twitter"></i></a></li>
         <li><a href="https://instagram.com/funvill"><i class="fa fa-instagram"></i></a></li>
@@ -25,3 +41,4 @@
       </ul>
     </div>
   </div>
+</div>
