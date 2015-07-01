c165311256a1ed5fc60a75f679a7052bb6f32fae
Steven Smethurst <steven@abluestar.com>
2015-07-01 03:01:11 -0700

    Cleaned up the 404 page

--- door.abluestar.com/tp_404.php
+++ door.abluestar.com/tp_404.php
@@ -1,5 +1,8 @@
-<div style='margin-bottom: 80px;' ></div>
 <div class='page'>
-  <h1>404 door not found</h1>
-  <p>There was an error and we can not find this door. </p>
+  <div class="jumbotron">
+    <div class="container">
+      <h1>404 door not found</h1>
+      <p>There was an error and we can not find this door. </p>
+    </div>
+  </div>
 </div>
